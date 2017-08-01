b0a56d65c5baa17fa780075cc249c202d31a78b8
Marvin Wiegand <mwiegand@de.ibm.com>
2017-08-01 11:14:18 +0200

    inject header navbar and improve responsive sticky elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ProfileComponent} from "./pages/profile.component";
 import {LearnComponent} from "./pages/learn.component";
 import {DataService} from "./shared/services/data.service";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {HeaderComponent} from "./core/header/header.component";
 
 @NgModule({
   imports: [
@@ -35,6 +36,7 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   ],
   declarations: [
     AppComponent,
+    HeaderComponent,
     ProfileComponent,
     LearnComponent,
   ],
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,25 @@
+import {Component} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
+
+
+@Component({
+  selector: 'head-nav',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+
+export class HeaderComponent {
+  routeLinks: any[];
+  activeLink = '';
+
+  constructor(private router: Router) {
+    this.routeLinks = [
+      {label: 'Profile', link: ''},
+      {label: 'Learn', link: 'learn'}];
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activeLink = this.routeLinks.find(route => event.url === "/" + route.link).link;
+      }
+    });
+  }
+}
