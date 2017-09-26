d654aa150bf5b091b516b49ff8f91878c1105737
Marvin Wiegand <mwiegand@de.ibm.com>
2017-09-26 15:09:39 +0200

    removes duplicate router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import 'rxjs/add/operator/map';
-import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +7,5 @@ import {NavigationEnd, Router} from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  routeLinks:any[];
-  activeLink = '';
-  constructor(private router: Router) {
-    this.routeLinks = [
-      {label: 'Profile', link: ''},
-      {label: 'Learn', link: 'learn'}];
-    router.events.subscribe((event) => {
-      if(event instanceof NavigationEnd){
-        this.activeLink = this.routeLinks.find(route => event.url === "/"+route.link).link;
-      }
-    });
-  }
+  constructor() {}
 }
